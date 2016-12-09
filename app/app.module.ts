f2843a65a47f660ac3fc17d162ee9fba1913ea95
Vikram Raj <vikramraj1991@gmail.com>
2016-12-09 11:56:30 +0530

    Convert our service and components to use Angular's HTTP service.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,26 +2,33 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule }  from '@angular/router';
+import { HttpModule }    from '@angular/http'
 
-import { AppComponent }        from './app.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { HeroesComponent }     from './heroes.component';
-import { DashboardComponent }  from './dashboard.component';
-import { HeroService }         from './hero.service';
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppComponent }         from './app.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroesComponent }      from './heroes.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroService }          from './hero.service';
 import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule,
   ],
 
   declarations: [ 
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent,
-    DashboardComponent ],
+    HeroesComponent, ],
   
   providers: [
     HeroService
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -1,12 +1,29 @@
-import { Injectable } from "@angular/core"
+import { Injectable } from "@angular/core";
+import { Headers, Http } from '@angular/http';
 
-import { Hero } from "./hero"
-import { HEROES } from "./mock-heroes"
+import 'rxjs/add/operator/toPromise';
+
+import { Hero } from "./hero";
 
 @Injectable()
 export class HeroService {
+
+    private heroesUrl = 'api/heroes';
+    constructor(private http: Http){};
+
     getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+                .toPromise()
+                .then(response => {
+                    return response.json().data as Hero[]
+                })
+                .catch(this.handleError);
+    }
+
+
+    private handleError(error: any): Promise<any> {
+        console.error('An error occurred', error); // for demo purposes only
+        return Promise.reject(error.message || error);
     }
 
     getHero(id: number): Promise<Hero> {
